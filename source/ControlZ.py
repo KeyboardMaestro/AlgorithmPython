ab1329b726d9be0699a6ee6360ea0fc5cd76f954
judepark <psw0616a@naver.com>
2023-02-03 13:52:54 +1100

    format update

--- source/ControlZ.py
+++ source/ControlZ.py
@@ -2,14 +2,12 @@ def solution(s):
     str1 = '10 20 Z 30'
     answer = 0
     for a in str1.split():
-        print(a)
         if a == 'Z' :
             answer -= temp
         else:
             answer += int(a)
             temp = int(a)
     return answer
-
 def solution2(s):
     stack = []
     for sub in s.split():

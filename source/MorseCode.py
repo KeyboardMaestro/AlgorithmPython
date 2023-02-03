df39ac062a123907fe1aaf6643d83377909c6009
judepark <psw0616a@naver.com>
2023-02-03 14:22:09 +1100

    Solved

---
+++ source/MorseCode.py
@@ -0,0 +1,13 @@
+def solution(letter):
+    answer = ''
+    morse = {
+        '.-': 'a', '-...': 'b', '-.-.': 'c', '-..': 'd', '.': 'e', '..-.': 'f',
+        '--.': 'g', '....': 'h', '..': 'i', '.---': 'j', '-.-': 'k', '.-..': 'l',
+        '--': 'm', '-.': 'n', '---': 'o', '.--.': 'p', '--.-': 'q', '.-.': 'r',
+        '...': 's', '-': 't', '..-': 'u', '...-': 'v', '.--': 'w', '-..-': 'x',
+        '-.--': 'y', '--..': 'z'
+    }
+    for code in letter.split(" "):
+        answer += morse.get(code)
+    return answer
+# Resource : https://school.programmers.co.kr/learn/courses/30/lessons/120838

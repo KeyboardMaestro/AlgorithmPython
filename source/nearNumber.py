0c93133d45ca0623e11b9a591b6c8e555958f522
judepark <psw0616a@naver.com>
2023-02-11 22:30:32 +1100

    The error fixed.

--- source/nearNumber.py
+++ source/nearNumber.py
@@ -1,7 +1,9 @@
 def solution(array, n):
     data = []
+    array = sorted(array)
     for i in array:
         data.append(abs(i-n))
-    minumum = data.index(min(data))
-    return array[minumum]
+    minimum = min(data)
+    print(array)
+    return array[data.index(minimum)]
 # Resource : https://school.programmers.co.kr/learn/courses/30/lessons/120890

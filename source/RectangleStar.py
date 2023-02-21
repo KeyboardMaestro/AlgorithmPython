d4e703493e40208c9790e01fdc0c1800d932b1a2
Sungwon Jude Park <psw0616a@naver.com>
2023-02-21 23:59:13 +1100

    Solved

---
+++ source/RectangleStar.py
@@ -0,0 +1,6 @@
+a, b = map(int, input().strip().split(' '))
+for horizon in range(0,b):
+    for vertical in range(0,a):
+        print('*', end='')
+    print('')
+# Resource : https://school.programmers.co.kr/learn/courses/30/lessons/12969

2b4aebb00ee92fe2435b02d6cc8f26f1ce9f36f8
Sungwon Jude Park <psw0616a@naver.com>
2023-03-06 23:36:51 +1100

    Trying

---
+++ source/SecretMap.py
@@ -0,0 +1,13 @@
+def solution(n, arr1, arr2):
+    answer = []
+    for idx in range(0, n):
+        temp = ''
+        for bit in str(bin(arr1[idx] | arr2[idx]))[2:]:
+            if(bit == '1'):
+                temp += '#'
+            else:
+                temp += ' '
+        if(len(temp) < n):
+            temp.join(' '*(n - len(temp)))
+        answer.append(temp)
+    return answer
